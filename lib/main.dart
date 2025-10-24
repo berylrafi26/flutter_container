e6ca5091dfbb19dff5e456de365b6c1cf7dd0408
berylrafi26 <berylrafi3@gmail.com>
2025-10-24 12:05:12 +0700

    buat void awal

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,7 @@
 import 'package:flutter/material.dart';
 
-Void main (){
-  runApp(MyApp());
-}
-
-class MyWidget extends StatelessWidget{
-  const MyWidget({super.key});
-
-  Widget build(BuildContext Context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Ini Title Application",
-      home: Home(),
-    );//Material
-  }
-}
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("judul"),
-        backgroundColor: Colors.blue,
-        actions: [
-          Icon(Icons.play_arrow,)
-        ],
-      ),
-    );
-  }
+  
 }
