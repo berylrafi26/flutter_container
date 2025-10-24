56e8ba126e6e3acff57205d36b9b05f433e560ad
berylrafi26 <berylrafi3@gmail.com>
2025-10-24 16:50:45 +0700

    border

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,75 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Ini Beryl',
+      title: 'Ini Title Application',
+      home: MyWidget(),
     );
   }
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
-        backgroundColor: Colors.pink,
-        
+        title: const Text('My App'),
+        backgroundColor: Colors.blue,
+        actions: const [
+          Icon(Icons.play_arrow),
+          SizedBox(width: 10),
+          Icon(Icons.access_alarm),
+          SizedBox(width: 10),
+          Icon(Icons.person),
+          SizedBox(width: 20),
+        ],
       ),
-      
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 200,
+              width: 200,
+              margin: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                gradient: const LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [Colors.blue, Colors.red],
+                ),
+                border: Border.all(color: Colors.black12),
+              ),
+            ),
+            Container(
+              height: 200,
+              width: 200,
+              margin: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                gradient: const LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [Colors.blue, Colors.red],
+                ),
+                border: Border.all(color: Colors.black12),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-//run app 
-// material app
-
-//scafold
